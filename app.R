c56f89a2d020f48b2cf0cdc53ffcfe136ac899a9
isajensen <cph-ij92@cphbusiness.dk>
2025-03-03 15:55:17 +0100

    Update app.R

--- app.R
+++ app.R
@@ -7,29 +7,30 @@ library(DT)
 library(mongolite)
 library(ggsoccer)
 
-# Forbindelse til MongoDB
+# Opret forbindelse til MongoDB-databaser
 db_games <- mongo(collection = "games", db = "wyscout", url = "mongodb://localhost:27017")
 db_matches <- mongo(collection = "matches", db = "wyscout", url = "mongodb://localhost:27017")
 
-# Hent skuddata 
-query <- '{"$or": [{"type.primary": "shot"}, {"type.secondary": "shot"}]}'
-fields <- '{"matchId": 1, "team": 1, "shot": 1, "player.name": 1, "_id": 0}'
-shots_data <- db_games$find(query, fields = fields)
+# Hent skuddata fra databasen
+query_skuddata <- '{"$or": [{"type.primary": "shot"}, {"type.secondary": "shot"}]}'
+fields_skuddata <- '{"matchId": 1, "team": 1, "shot": 1, "player.name": 1, "_id": 0}'
+skuddata <- db_games$find(query_skuddata, fields = fields_skuddata)
 
-# Hent kampdata
-matches_data <- db_matches$find('{}', '{ "_id": 1, "competitionId": 1, "seasonId": 1, "date": 1 }')
+# Hent kampdata fra databasen
+kampdata <- db_matches$find('{}', '{ "_id": 1, "competitionId": 1, "seasonId": 1, "date": 1 }')
 
-# Unnest skuddata
-shots_flat <- shots_data %>%
+# Fladgør skuddata for nemmere håndtering
+skuddata_flad <- skuddata %>%
   unnest(team, names_sep = "_") %>%
   unnest(player, names_sep = "_") %>%
   unnest(shot, names_sep = "_")
 
-# Merge skuddata med kampdata
-matches_data <- matches_data %>% rename(matchId = `_id`)
+# Omdøb kolonne for at matche i merge
+kampdata <- kampdata %>% rename(matchId = `_id`)
 
-shots_merged <- shots_flat %>%
-  left_join(matches_data, by = "matchId") %>%
+# Flet skuddata med kampdata og tilføj liga- og sæsoninformation
+skuddata_samlet <- skuddata_flad %>%
+  left_join(kampdata, by = "matchId") %>%
   mutate(
     league = ifelse(competitionId == 635, "Holland", "Polen"),
     season = case_when(
@@ -39,23 +40,23 @@ shots_merged <- shots_flat %>%
   )
 
 # Hent data til "Skud i en kamp"
-query_skud <- '{"$or": [{"type.primary": "shot"}, {"type.secondary": "shot"}]}'
-fields_skud <- '{"matchId": 1, "location": 1, "team": 1, "shot": 1, "type": 1, "_id": 0}'
-kamp_data <- db_games$find(query_skud, fields = fields_skud)
+query_skud_kamp <- '{"$or": [{"type.primary": "shot"}, {"type.secondary": "shot"}]}'
+fields_skud_kamp <- '{"matchId": 1, "location": 1, "team": 1, "shot": 1, "type": 1, "_id": 0}'
+skuddata_kamp <- db_games$find(query_skud_kamp, fields = fields_skud_kamp)
 
 # Hent matchinformation til "Skud i en kamp"
-matches_data_skud <- db_matches$find('{}', fields = '{"_id": 1, "label": 1, "competitionId": 1, "seasonId": 1}')
+kampinfo_skud <- db_matches$find('{}', fields = '{"_id": 1, "label": 1, "competitionId": 1, "seasonId": 1}')
 
-# Datarensning og merging til "Skud i en kamp"
-kamp_data_merged <- kamp_data %>%
-  left_join(matches_data_skud, by = c("matchId" = "_id")) %>%
+# Rens og flet data til "Skud i en kamp"
+skuddata_kamp_samlet <- skuddata_kamp %>%
+  left_join(kampinfo_skud, by = c("matchId" = "_id")) %>%
   mutate(
     location_x = location$x,
     location_y = location$y,
     team_name = team$name,
     shot_isGoal = shot$isGoal,
     shot_onTarget = shot$onTarget,
-    kamp_label = label  # Tilføj kamp_label fra matches_data_skud
+    kamp_label = label  # Tilføj kamp_label fra kampinfo_skud
   ) %>%
   select(-location, -team, -shot) %>%
   mutate(
@@ -63,12 +64,12 @@ kamp_data_merged <- kamp_data %>%
   )
 
 # Hent data til "Afleveringer"
-query_passes <- '{"type.primary": "pass"}'
-fields_passes <- '{"matchId": 1, "team": 1, "pass": 1, "player": 1, "_id": 0}'
-passes_data <- db_games$find(query_passes, fields = fields_passes)
+query_afleveringer <- '{"type.primary": "pass"}'
+fields_afleveringer <- '{"matchId": 1, "team": 1, "pass": 1, "player": 1, "_id": 0}'
+afleveringsdata <- db_games$find(query_afleveringer, fields = fields_afleveringer)
 
-# Unnest og rense afleveringsdata
-passes_flat <- passes_data %>%
+# Fladgør og ren afleveringsdata
+afleveringsdata_flad <- afleveringsdata %>%
   mutate(
     team_name = team$name,
     player_name = player$name,
@@ -76,9 +77,9 @@ passes_flat <- passes_data %>%
   ) %>%
   select(-team, -player, -pass)
 
-# Merge afleveringsdata med kampdata
-passes_merged <- passes_flat %>%
-  left_join(matches_data, by = "matchId") %>%
+# Flet afleveringsdata med kampdata
+afleveringsdata_samlet <- afleveringsdata_flad %>%
+  left_join(kampdata, by = "matchId") %>%
   mutate(
     league = ifelse(competitionId == 635, "Holland", "Polen"),
     season = case_when(
@@ -106,8 +107,8 @@ ui <- fluidPage(
     tabPanel("Mål og xG",
              sidebarLayout(
                sidebarPanel(
-                 selectInput("input_league", "Vælg liga:", choices = c("Alle", unique(shots_merged$league))),
-                 selectInput("input_season", "Vælg sæson:", choices = c("Alle", unique(shots_merged$season))),
+                 selectInput("input_league", "Vælg liga:", choices = c("Alle", unique(skuddata_samlet$league))),
+                 selectInput("input_season", "Vælg sæson:", choices = c("Alle", unique(skuddata_samlet$season))),
                  selectInput("input_team", "Vælg hold:", choices = "Alle"),
                  sliderInput("input_num_players", "Antal spillere:", min = 5, max = 50, value = 20, step = 1),
                  downloadButton("download_player_data", "Download Data", class = "btn-primary")
@@ -121,7 +122,7 @@ ui <- fluidPage(
     tabPanel("Afleveringer",
              sidebarLayout(
                sidebarPanel(
-                 selectInput("liga", "Vælg liga:", choices = unique(passes_merged$league)),
+                 selectInput("liga", "Vælg liga:", choices = unique(afleveringsdata_samlet$league)),
                  selectInput("team_name", "Vælg hold:", choices = NULL),
                  selectInput("matchId", "Vælg kamp:", choices = NULL),
                  actionButton("compare", "Se data"),
@@ -136,7 +137,7 @@ ui <- fluidPage(
              fluidRow(
                column(3, 
                       wellPanel(
-                        selectInput("liga_skud", "Vælg liga:", choices = unique(kamp_data_merged$liga)),
+                        selectInput("liga_skud", "Vælg liga:", choices = unique(skuddata_kamp_samlet$liga)),
                         selectInput("hold_skud", "Vælg hold:", choices = NULL),
                         selectInput("kamp_skud", "Vælg kamp:", choices = NULL)
                       )
@@ -152,21 +153,21 @@ ui <- fluidPage(
 # Server
 server <- function(input, output, session) {
   # Mål og xG
-  filtered_shots <- reactive({
-    shots_merged %>%
+  filtrerede_skud <- reactive({
+    skuddata_samlet %>%
       filter((input$input_league == "Alle" | league == input$input_league),
              (input$input_season == "Alle" | season == input$input_season))
   })
   
   observe({
-    available_teams <- unique(filtered_shots()$team_name)
+    tilgængelige_hold <- unique(filtrerede_skud()$team_name)
     updateSelectInput(session, "input_team",
-                      choices = c("Alle", available_teams),
-                      selected = ifelse(input$input_team %in% available_teams, input$input_team, "Alle"))
+                      choices = c("Alle", tilgængelige_hold),
+                      selected = ifelse(input$input_team %in% tilgængelige_hold, input$input_team, "Alle"))
   })
   
-  player_summary <- reactive({
-    filtered_shots() %>%
+  spilleroversigt <- reactive({
+    filtrerede_skud() %>%
       filter(input$input_team == "Alle" | team_name == input$input_team) %>%
       group_by(player_name) %>%
       summarise(total_goals = sum(shot_isGoal, na.rm = TRUE),
@@ -176,7 +177,7 @@ server <- function(input, output, session) {
   })
   
   output$goals_xg_plot <- renderPlot({
-    ggplot(player_summary(), aes(x = reorder(player_name, total_goals), y = total_goals)) +
+    ggplot(spilleroversigt(), aes(x = reorder(player_name, total_goals), y = total_goals)) +
       geom_bar(stat = "identity", fill = "#0073C2", color = "black", width = 0.6) +
       geom_point(aes(y = total_xG, color = "xG sum"), size = 5, alpha = 0.8) +
       scale_color_manual(name = "", values = c("xG sum" = "#EFC000FF")) +
@@ -201,7 +202,7 @@ server <- function(input, output, session) {
   })
   
   output$player_data_table <- renderDT({
-    datatable(player_summary(), options = list(pageLength = 10, scrollX = TRUE))
+    datatable(spilleroversigt(), options = list(pageLength = 10, scrollX = TRUE))
   })
   
   output$download_player_data <- downloadHandler(
@@ -209,42 +210,42 @@ server <- function(input, output, session) {
       paste("player_data_", Sys.Date(), ".csv", sep = "")
     },
     content = function(file) {
-      write.csv(player_summary(), file, row.names = FALSE)
+      write.csv(spilleroversigt(), file, row.names = FALSE)
     }
   )
   
   # Afleveringer
   observe({
-    updateSelectInput(session, "liga", choices = unique(passes_merged$league))
+    updateSelectInput(session, "liga", choices = unique(afleveringsdata_samlet$league))
   })
   
   observeEvent(input$liga, {
-    hold_choices <- passes_merged %>%
+    hold_valg <- afleveringsdata_samlet %>%
       filter(league == input$liga) %>%
       pull(team_name) %>%
       unique()
     
-    updateSelectInput(session, "team_name", choices = hold_choices)
+    updateSelectInput(session, "team_name", choices = hold_valg)
   })
   
   observeEvent(input$team_name, {
-    kamp_choices <- passes_merged %>%
+    kamp_valg <- afleveringsdata_samlet %>%
       filter(team_name == input$team_name) %>%
       pull(matchId) %>%
       unique()
     
-    updateSelectInput(session, "matchId", choices = kamp_choices)
+    updateSelectInput(session, "matchId", choices = kamp_valg)
   })
   
   observeEvent(input$compare, {
     req(input$matchId)
     
     # Filtrer data baseret på valgt kamp
-    filtered_data <- passes_merged %>%
+    filtrerede_data <- afleveringsdata_samlet %>%
       filter(matchId == input$matchId)
     
     # Beregn præcise og upræcise afleveringer
-    pass_summary <- filtered_data %>%
+    afleveringsoversigt <- filtrerede_data %>%
       group_by(team_name) %>%
       summarise(
         good = sum(pass_accurate, na.rm = TRUE),
@@ -257,7 +258,7 @@ server <- function(input, output, session) {
     
     # Plot
     output$barPlot <- renderPlot({
-      ggplot(pass_summary, aes(x = team_name, y = count, fill = pass_type)) +
+      ggplot(afleveringsoversigt, aes(x = team_name, y = count, fill = pass_type)) +
         geom_bar(stat = "identity", position = "stack") +
         geom_text(aes(label = sprintf("%.0f %%", percentage), y = count / 2), 
                   color = "black", size = 5, fontface = "bold") +
@@ -283,37 +284,37 @@ server <- function(input, output, session) {
   
   # Skud i en kamp
   observeEvent(input$liga_skud, {
-    hold_choices <- kamp_data_merged %>%
+    hold_valg <- skuddata_kamp_samlet %>%
       filter(liga == input$liga_skud) %>%
       pull(team_name) %>%
       unique()
     
-    updateSelectInput(session, "hold_skud", choices = hold_choices)
+    updateSelectInput(session, "hold_skud", choices = hold_valg)
   })
   
   observeEvent(input$hold_skud, {
-    kamp_choices <- kamp_data_merged %>%
+    kamp_valg <- skuddata_kamp_samlet %>%
       filter(team_name == input$hold_skud) %>%
       pull(kamp_label) %>%
       unique()
     
-    updateSelectInput(session, "kamp_skud", choices = kamp_choices)
+    updateSelectInput(session, "kamp_skud", choices = kamp_valg)
   })
   
   output$skudPlot <- renderPlot({
     req(input$kamp_skud) 
     
-    data_filtered <- kamp_data_merged %>%
+    data_filtreret <- skuddata_kamp_samlet %>%
       filter(kamp_label == input$kamp_skud)
     
-    hold1 <- unique(data_filtered$team_name)[1]
-    hold2 <- unique(data_filtered$team_name)[2]
+    hold1 <- unique(data_filtreret$team_name)[1]
+    hold2 <- unique(data_filtreret$team_name)[2]
     
-    data_filtered <- data_filtered %>%
+    data_filtreret <- data_filtreret %>%
       mutate(location_x = ifelse(team_name == hold1, location_x, 100 - location_x))
     
-    ggplot(data_filtered, aes(x = location_x, y = location_y, color = team_name, 
-                              shape = shot_onTarget, size = shot_isGoal)) +
+    ggplot(data_filtreret, aes(x = location_x, y = location_y, color = team_name, 
+                               shape = shot_onTarget, size = shot_isGoal)) +
       annotate_pitch(fill = "seagreen", alpha = 0.5, colour = "white") + 
       geom_point(alpha = 0.8) +
       scale_color_manual(values = c("red", "blue")) + 
@@ -330,8 +331,3 @@ shinyApp(ui = ui, server = server)
 
 library(rsconnect)
 deployApp()
-
-
-
-
-
